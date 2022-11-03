451b849c68593fe499b01deb61be452edebf9ef0
Zakaria Kortam <65579842+zkortam@users.noreply.github.com>
2022-11-02 22:10:36 -0700

    Update MatricesIO.cpp

--- MatricesIO.cpp
+++ MatricesIO.cpp
@@ -8,41 +8,55 @@
 
 using namespace std;
 
-	int main() {
-		
-	double a[16], b[16], c[16], d[16]; 
-	ofstream output;
-	ifstream input;
-	input.open("datafile.txt");
-	for (int i=0;i<15;i++){
-		input>> a[i];
-		input>> b[i];
-	}
-	for (int i=0;i<15;i++){
-		
-		double tempvala = a[i];
-		double tempvalb = b[i];
-		c[i] = tempvala + tempvalb;
-		d[i] = pow (c[i], 2);
-	}
-	
-	output.open("outputmatrix.txt");
-	
-	
-	for(int j=0; j<4; j++){
-		for (int i=0;i<4;i++){
-		output<<d[i]<<" ";
-	}
-	output<<endl;
-	}
-	
-		output<<endl;
-			output<<endl;
-				output<<endl;
-		for(int j=0; j<4; j++){
-		for (int i=0;i<4;i++){
-		output<<c[i]<<" ";
-	}
-	output<<endl;
-	}
+int main() {
+    
+    double a[16], b[16], c[16], d[16]; 
+    ofstream output;
+    ifstream input;
+    
+    input.open("datafile.txt");
+    
+    for (int i = 0; i < 16; i++) {
+        input >> a[i];
+    }
+    
+    for (int i = 0; i < 16; i++) {
+        input>> b[i];
+    }
+    
+    for (int i = 0; i < 16; i++) {
+        c[i] = a[i] + b[i];
+        d[i] = pow (c[i], 2);
+    }
+    
+    output.open("outputmatrix.txt");
+
+
+    int n = 4;
+    int i = 0;
+    for (int j = 0; j < 4; j++) {
+        for (int k = 0; k < 4; k++) {
+            output << d[i] << " ";
+            i++;
+        }
+        output << endl;
+        n = n + 4;
+    }
+    
+    // blank lines for readability
+    output<<endl;
+    output<<endl;
+    output<<endl;
+        
+    // reset counters
+    n = 4;
+    i = 0;
+    for (int j = 0; j < 4; j++) {
+        for (int k = 0; k < 4; k++) {
+            output << c[i] << " ";
+            i++;
+        }
+        output << endl;
+        n = n + 4;
+    }
 }
